a12c9f490a2ba3dd338366dd1ec630d5aab8256b
Davide Baldelli <davbaldelli@gmail.com>
2021-03-24 22:22:08 +0100

    riordinati file test

---
+++ src/test/davide/Main.java
@@ -0,0 +1,84 @@
+package test.davide;
+
+import enemies.EnemyFactory;
+import enemies.pool.EnemiesPoolImpl;
+import enemies.pool.EnemiesPoolMover;
+import towers.Pair;
+import towers.TowerBase;
+import towers.TowerFactory;
+
+
+public class Main {
+	
+	private static final Pair<Double, Double> START_POS =  new Pair<>(0.0,0.0);
+	
+	 public static void main(String[] args) {
+		 
+		 EnemiesPoolImpl pool = new EnemiesPoolImpl(new MapInteractorImpl());
+		 
+		 EnemyFactory eFactory = new EnemyFactory();
+		 
+		 pool.addEnemy(eFactory.getEnemy1(START_POS));
+		 //pool.addEnemy(eFactory.getEnemy2(START_POS));
+		 
+		 TowerFactory tFactory = new TowerFactory();
+		 TowerBase tz1 = tFactory.getTowerZone1(pool, new Pair<>(5.0,5.0));
+		 TowerBase tz2 = tFactory.getTowerZone2(pool, new Pair<>(0.0,0.0));
+		 
+		 Thread eThread = new EnemiesThread(pool);
+		 Thread tThread1 = new TowerThread(tz1);
+		 Thread tThread2 = new TowerThread(tz2);
+		 
+		 eThread.start();
+		 tThread1.start();
+		 //tThread2.start();
+		 
+		 
+	 }
+	 
+	 static class EnemiesThread extends Thread {
+		 
+		 private final EnemiesPoolMover mover;
+		 
+		 public EnemiesThread(EnemiesPoolMover pool) {
+			 this.mover = pool;
+		 }
+		 
+		 @Override
+		 public void run(){
+			 while(true){
+				 try {
+					 sleep(1000L);
+					 mover.moveEnemies();
+				 } catch (InterruptedException ex) {
+					 System.out.println(ex.getMessage());
+				 }
+			 }
+		 }
+	 }
+	 
+	 static class TowerThread extends Thread {
+		 
+		 private final TowerBase tower;
+		 
+		 public TowerThread(TowerBase tower){
+			 this.tower = tower;
+		 }
+		 
+		 @Override
+		 public void run() {
+			 while(true){
+				 try {
+					 sleep(1000L * tower.getShootSpeed());
+					 if (tower.shoot().isEmpty()) {
+						 System.out.println("No more enemies around...");
+					 }
+				 } catch (Exception ex) {
+					 System.out.println(ex.getMessage());
+				 }
+			 }
+		 }
+		 
+	 }
+
+}
